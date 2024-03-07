63e3b8f07d88c69788a3ac702daa86103eaff8a1
AbdulrhmanSoliman <abdo.soliman156@gmail.com>
2024-03-07 14:46:43 +0200

    fix

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ questionsName.forEach((question) => {
       this.nextElementSibling.classList.toggle("hidden");
     // Accessing each icon and change its src and alt 
       if(this.lastElementChild.alt == "icon-plus"){
-        this.lastElementChild.src = "/assets/images/icon-minus.svg";
+        this.lastElementChild.src = "icon-minus.svg";
         this.lastElementChild.alt = "icon-minus";
       }else{
-        this.lastElementChild.src = "/assets/images/icon-plus.svg";
+        this.lastElementChild.src = "icon-plus.svg";
         this.lastElementChild.alt = "icon-plus";
       }
   });
